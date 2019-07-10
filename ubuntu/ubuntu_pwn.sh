6abb1f13901e7fd620b1e93143f80f1090d217fd
pwn xyzz <pwnxyzz@pwndeMacBook-Pro.local>
2019-07-10 17:11:35 +0800

    linux_easy_install

--- ubuntu/ubuntu_pwn.sh
+++ ubuntu/ubuntu_pwn.sh
@@ -1,3 +1,4 @@
 sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list;
 apt-get update;
 apt-get install wget -y;
+wget -q -O- https://raw.githubusercontent.com/chnxyzz/linux_install/master/pwn_apt/pwn_add32.sh | sh;
