7b0d72823ec1ddc8cdd2994b6bbfb648033842f7
pwn xyzz <pwnxyzz@pwndeMacBook-Pro.local>
2019-07-18 07:34:41 +0800

    linux_easy_install

---
+++ useful/template.cpp
@@ -0,0 +1,26 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<bitset>
+#include<cstdlib>
+#include<cmath>
+#include<set>
+#include<list>
+#include<deque>
+#include<map>
+#include<queue>
+using namespace std;
+typedef long long ll;
+const double PI = acos(-1.0);
+const double eps = 1e-6;
+const int INF = 1000000000;
+const int maxn = 100;
+ 
+int main() {
+    return 0;
+}
+
