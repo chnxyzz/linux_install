66abbbeb7e15794a03f8b9d2dde2918e7b0b21b3
pwn xyzz <pwnxyzz@pwndeMacBook-Pro.local>
2019-10-29 07:06:54 +0800

    new

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -1,6 +1,7 @@
 sudo ${PM} update;
 sudo ${PM} install curl -y;
 sudo curl -sSL https://get.daocloud.io/docker | sh;
-sudo groupadd docker;
-sudo gpasswd -a ${USER} docker;
-sudo systemctl restart docker;
+sudo usermod -aG docker ${USER};
+#sudo groupadd docker;
+#sudo gpasswd -a ${USER} docker;
+#sudo systemctl restart docker;
