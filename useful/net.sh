7b15f8399c77a2bb99cc84245243d7272764dab1
pwn xyzz <pwnxyzz@pwndeMacBook-Pro.local>
2019-11-28 22:59:21 +0800

    new

--- useful/net.sh
+++ useful/net.sh
@@ -1,1 +1,7 @@
 ip=127.0.0.1 && port=1080 && export http_proxy="$ip:$port/" && export https_proxy="https://$ip:$port/" && git config --global http.proxy "http://$ip:$port" && git config --global http.proxy "http://$ip:$port"
+
+
+ip=10.1.0.17 && port=1080 && export http_proxy="$ip:$port/" && export https_proxy="https://$ip:$port/" && git config --global http.proxy "http://$ip:$port" && git config --global http.proxy "http://$ip:$port"
+
+
+
