208236a8e189f9a915766ce58171de88a01e0cbe
chnxyzz <3075264738@qq.com>
2019-12-03 21:21:40 +0800

    Update net.sh

--- useful/net.sh
+++ useful/net.sh
@@ -4,5 +4,7 @@ ip=127.0.0.1 && port=1080 && export http_proxy="$ip:$port/" && export https_prox
 ip=10.1.0.17 && port=1080 && export http_proxy="$ip:$port/" && export https_proxy="https://$ip:$port/" && git config --global http.proxy "http://$ip:$port" && git config --global http.proxy "http://$ip:$port"
 
 
-
 sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/source.list && sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/source.list
+
+
+
