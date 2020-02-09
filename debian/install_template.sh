a90176429a50d7cb2274f91dd8bde3c1d3fbfbbc
chnxyzz <3075264738@qq.com>
2020-02-09 11:44:13 +0800

    Update install_template.sh

--- debian/install_template.sh
+++ debian/install_template.sh
@@ -1,3 +1,4 @@
 sed -i 's/deb.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list;
 sed -i 's/security.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list;
+sed -i 's/security-cdn.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list;
 apt-get update;
