d4d7f923371f08f6d91795ec978b4747518903e6
chnxyzz <3075264738@qq.com>
2020-03-09 04:56:35 +0800

    Update nfs.sh

--- useful/nfs.sh
+++ useful/nfs.sh
@@ -3,3 +3,4 @@ mkdir /root/nfs;
 echo '/root/nfs 10.0.0.0/13(insecure,rw,all_squash,anonuid=0,anongid=0,no_root_squash,no_subtree_check,sync)'>> /etc/exports;
 sudo exportfs -vra;
 #sudo mount -t nfs -o resvport,locallocks ip:/root/nfs dir
+#mount -o nolock,resvport,rsize=131072,wsize=131072 ip:/root/nfs dir
