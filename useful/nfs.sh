960b6523c8cfa5df6263ecfb588dc1677bb5f2c7
chnxyzz <3075264738@qq.com>
2020-03-08 17:28:39 +0800

    Update nfs.sh

--- useful/nfs.sh
+++ useful/nfs.sh
@@ -1,5 +1,5 @@
 sudo apt-get install nfs-kernel-server -y;
 mkdir /root/nfs;
-echo '/root/nfs 10.0.0.0/13(insecure,rw,all_squash,anonuid=0,anongid=0,no_subtree_check)'>> /etc/exports;
+echo '/root/nfs 10.0.0.0/13(insecure,rw,all_squash,anonuid=0,anongid=0,no_root_squash,no_subtree_check,sync)'>> /etc/exports;
 sudo exportfs -vra;
-#sudo mount -t nfs -o resvport ip:/root/nfs dir
+#sudo mount -t nfs -o resvport,locallocks ip:/root/nfs dir
