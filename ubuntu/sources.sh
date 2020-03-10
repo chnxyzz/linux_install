46da930b67c37259eeb9e065b750a247daf7bf7e
chnxyzz <3075264738@qq.com>
2020-03-10 10:49:09 +0800

    Update sources.sh

--- ubuntu/sources.sh
+++ ubuntu/sources.sh
@@ -1,2 +1,3 @@
+sed -i "s/cn.archive.ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list;
 sed -i "s/archive.ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list;
 sed -i "s/security.ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list;
