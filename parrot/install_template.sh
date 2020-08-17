7a35089f7455b09193d4d993d3f43acad31846a9
pwn xyzz <pwnxyzz@pwndeMacBook-Pro.local>
2020-08-18 04:47:32 +0800

    new

--- parrot/install_template.sh
+++ parrot/install_template.sh
@@ -1,2 +1,3 @@
 sed -i "s/deb.parrotsec.org/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list.d/parrot.list;
+sed -i "s/deb.parrot.sh/mirrors.bfsu.edu.cn/g" /etc/apt/sources.list.d/parrot.list;
 apt-get update;
