153d6df203469c56eb35261047a9389238f8a99f
chnxyzz <28676240+chnxyzz@users.noreply.github.com>
2025-03-07 22:28:07 +0800

    Update docker_proxy.py

--- docker/docker_proxy.py
+++ docker/docker_proxy.py
@@ -1,5 +1,9 @@
 #encoding=utf-8
 import os
+import sys
+proxy_url="http://proxy.dopwn.cn:8080"
+if len(sys.argv)>2:
+  proxy_url = sys.argv[1]
 os.system('mkdir /etc/systemd/system/docker.service.d')
 templa1="""[Service]
  # NO_PROXY is optional and can be removed if not needed
@@ -7,7 +11,7 @@ templa1="""[Service]
  # For Proxy server which require username and password authentication, just add the proper username and password to the URL. (see example below)
 
  # Example without authentication
- Environment="HTTP_PROXY=http://proxy.dopwn.cn:8080" "NO_PROXY=localhost,127.0.0.0/8"
+ Environment=f"HTTP_PROXY={proxy_url}" "NO_PROXY=localhost,127.0.0.0/8"
 """
 templa2="""[Service]
  # NO_PROXY is optional and can be removed if not needed
@@ -15,7 +19,7 @@ templa2="""[Service]
  # For Proxy server which require username and password authentication, just add the proper username and password to the URL. (see example below)
 
  # Example without authentication
- Environment="HTTPS_PROXY=http://proxy.dopwn.cn:8080" "NO_PROXY=localhost,127.0.0.0/8"
+ Environment=f"HTTPS_PROXY={proxy_url}" "NO_PROXY=localhost,127.0.0.0/8"
 """
 f=open('/etc/systemd/system/docker.service.d/http-proxy.conf','w')
 f.write(templa1)
